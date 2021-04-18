3534ea793f65b0b423aafb8ec0f95cd3057c7195
liankui <yuekewin@gmail.com>
2021-04-18 19:58:37 +0800

    build: app go-zero api

---
+++ bookstore/go.mod
@@ -0,0 +1,11 @@
+module bookstore
+
+go 1.15
+
+require (
+	github.com/antlr/antlr4 v0.0.0-20210412152734-e404d26f6142 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/iancoleman/strcase v0.1.3 // indirect
+	github.com/tal-tech/go-zero v1.1.6
+	go.uber.org/automaxprocs v1.4.0 // indirect
+)
---
+++ bookstore/internal/logic/addlogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"bookstore/internal/svc"
+	"bookstore/internal/types"
+	"context"
+	"fmt"
+
+	"github.com/tal-tech/go-zero/core/logx"
+)
+
+type AddLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewAddLogic(ctx context.Context, svcCtx *svc.ServiceContext) AddLogic {
+	return AddLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *AddLogic) Add(req types.AddReq) (*types.AddResp, error) {
+	fmt.Printf("req:%v\n", req)
+
+	return &types.AddResp{}, nil
+}
